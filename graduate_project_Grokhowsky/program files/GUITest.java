813a4121ae68fb8a25991897b9fd98b70bba200c
Nick Grokhowsky <nickgrok@gmail.com>
2018-09-29 15:08:02 -0400

    Raster Analysis

---
+++ graduate_project_Grokhowsky/program files/GUITest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*; 
+import javax.swing.event.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import javax.imageio.ImageIO;
+
+
+class GUITest
+{
+	public static void main(String[]args)
+	{
+
+		//File f = new File("/home/nick/Documents/SUR5386/Final Project/images/nick.jpg");
+		File f = new File("/home/nick/Documents/SUR5386/Final Project/images/nick.jpg");
+		
+		
+		GUI2 veg = new GUI2();
+		veg.setVisible(true);
+
+	}
+}
